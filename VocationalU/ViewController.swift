114930ec8f2975f0d41a078efb4f349685af1e80
Darwie Fang <fangdb@bc.edu>
2020-04-15 12:38:33 +1000

    Initial Commit

---
+++ VocationalU/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VocationalU
+//
+//  Created by darwie fang on 15/4/20.
+//  Copyright © 2020 darwie fang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
